81b9c10d35f9697e8685b5dd3d3f9d229c0a60e8
Aliel <alielreyes@gmail.com>
2016-11-07 22:29:28 -0800

    EnemyAiMovement
    Added Coroutines for time delays, added enemy prototype texture for when
    I do rotations and only the left side timing is working correctly. Need
    to fix the right side timing delay recovery.

--- Assets/Scripts/AI/EnemyAi.cs
+++ Assets/Scripts/AI/EnemyAi.cs
@@ -3,23 +3,23 @@ using System.Collections;
 
 public class EnemyAi : MonoBehaviour {
 
-
+    #region Data
     //Timer
     [Space(5)]
     [Header("Choose When To Delay: ")]
     [Tooltip("Triggers right delay")]
-    public bool triggerRightPosDelay = false;
+    public bool triggerRightPosDelay = false; //Lets me know which check to toggle
     public bool triggerLeftPosDelay = false;
+    private bool checkLeft = false; //toggles on and off to make sure you only check for max reach once
+    private bool checkRight = false;
+
 
     [Space(5)]
     [Header("Input how long is the delay: ")]
     public float rightPosDelay = 0;
     public float leftPosDelay = 0;
 
-    private float passingTime = 0;
-    private const float leftTimeOffset = .8f;
-    private const float rightTimeOffset = .3f;
-    private float finalDelay;
+    private float timeSinceEnded = 0;
 
     //Calculate movement left to right
     [Space(5)]
@@ -34,7 +34,6 @@ public class EnemyAi : MonoBehaviour {
     private bool isLerping;
     public bool canMove = true;
     bool isGrounded = false;
-    bool runOnce = true;
 
     Vector2 startPos;
     Vector2 endPos;
@@ -42,11 +41,18 @@ public class EnemyAi : MonoBehaviour {
     Rigidbody2D rb;
 
     private float timeStartedLerping;
-
+    #endregion
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-
+        if (triggerLeftPosDelay)
+        {
+            checkLeft = true;
+        }
+        if (triggerRightPosDelay)
+        {
+            checkRight = true;
+        }
     }
 
     void FixedUpdate()
@@ -61,69 +67,92 @@ public class EnemyAi : MonoBehaviour {
 
     #region Game Functions
 
-    void RightPosDelay()
+    #region Timer Delays
+    //Delay time for when you reach maxLeft or maxRight
+    IEnumerator Delay(float delay)
     {
-        if (runOnce)
-        { 
-            finalDelay = rightPosDelay + Time.time;
-            runOnce = false;
-        }
-        else if (Time.time >= finalDelay)
+        Debug.Log("Coroutine Start: ");
+        yield return new WaitForSeconds(delay);
+        Debug.Log("Coroutine End: ");
+        if (triggerLeftPosDelay)
         {
-            isLerping = true;
-            runOnce = true;
-            passingTime = Time.time + rightTimeOffset; //reset// time offset
+            timeStartedLerping = Time.time; //initialize from the far left
+            StartCoroutine(CheckLeftToggle(1));
         }
+        else if(triggerRightPosDelay)
+        {
+            timeStartedLerping = Time.time - timeSinceEnded; //offSet to start from the final right position
+            StartCoroutine(CheckRightToggle(1));
+        }
+        isLerping = true;
     }
 
-    void LeftPosDelay()
+    //This toggles checkleft or right to not get stuck in an infinite checkLoop while the player is at the maxRight or maxLeft
+    IEnumerator CheckRightToggle(float time)
     {
-        if (runOnce)
-        {
-            finalDelay = leftPosDelay + Time.time;
-            runOnce = false;
-        }
-        else if (Time.time >= finalDelay)
-        {
-            isLerping = true;
-            runOnce = true;
-            passingTime = Time.time - leftTimeOffset; //reset// time offset
-            Debug.Log("PassingTime Left: " + passingTime);
-        }
+        Debug.Log("Checking is off");
+        yield return new WaitForSeconds(time);
+
+        Debug.Log("Checking is on");
+        checkRight = true;
     }
 
+    IEnumerator CheckLeftToggle(float time)
+    {
+        Debug.Log("Checking is off");
+        yield return new WaitForSeconds(time);
+        Debug.Log("Checking is on");
+        checkLeft = true;
+
+    }
+    #endregion
 
     void Movement()
     {
         if (isLerping)
         {
-            float timeSinceStarted = passingTime - (Time.time - timeStartedLerping);
-            Debug.Log("timeSinceStarted: " + timeSinceStarted);
-            float percentageComplete = (timeSinceStarted / timeTakenDuringLerp);
+            float timeSinceStarted = Time.time - timeStartedLerping; //This subtraction means it will start from the point where you leave it on when you start lerp = Start lerp when I decide.
+            //Debug.Log("timeSinceStarted: " + timeSinceStarted);
+            float percentageComplete = (timeSinceStarted / timeTakenDuringLerp); //calculate the ammount it takes to reach your destination over a period of time.
+            //Debug.Log("percentageComplete: " + percentageComplete);
 
 
-            transform.position = Vector2.Lerp(startPos, new Vector2(endPos.x + distance, endPos.y), Mathf.PingPong(percentageComplete, 1f));
-            Debug.Log(Mathf.PingPong(percentageComplete, 1f));
-            if (Mathf.PingPong(percentageComplete, 1f) >= .9f && triggerRightPosDelay) //check right first// this is stopping the delay. Starting from where 1 left off 
+            transform.position = Vector2.Lerp(startPos, new Vector2( endPos.x + distance, endPos.y), Mathf.PingPong(percentageComplete, 1f)); //0 init, 1 final 
+            
+
+           // Debug.Log(Mathf.PingPong(percentageComplete, 1f));
+            if (Mathf.PingPong(percentageComplete, 1f) >= .99f && checkRight) //check right first// this is stopping the delay. Starting from where 1 left off 
             {
+
+
                 Debug.Log("Max Right Delay");
                 //stop player movement
                 isLerping = false;
+
+                //Stop checking 
+                checkRight = false;
+
+                //tracks the timer of when you reach max right
+                timeSinceEnded = Time.time;
+                Debug.Log("Timer since ended: " + timeSinceEnded);
+
+                //Start delay
+                StartCoroutine(Delay(rightPosDelay));
+
             }
-            else if (Mathf.PingPong(percentageComplete, 1f) <= 0.1f && triggerLeftPosDelay) //this is stopping the delay. Starting from where 0 left off
+
+            if (Mathf.PingPong(percentageComplete, 1f) <= 0.01f && checkLeft) //this is stopping the delay. Starting from where 0 left off
             {
                 Debug.Log("Max Left Delay");
                 //stop player movement
                 isLerping = false;
+
+                //StopChecking
+                checkLeft = false;
+
+                //Start delay
+                StartCoroutine(Delay(leftPosDelay));
             }
-        }
-        else if (triggerRightPosDelay) //if you are on ground and you press one delay or both delays
-        {
-            RightPosDelay();
-        }
-        else if (triggerLeftPosDelay)
-        {
-            LeftPosDelay();
         }
     }
 
@@ -131,10 +160,10 @@ public class EnemyAi : MonoBehaviour {
     void StartLerping()
     {
         isLerping = true;
-        timeStartedLerping = Time.time;
+        timeStartedLerping = Time.time; //.52f
 
-        startPos = transform.position;
-        endPos =  transform.position;
+        startPos = transform.position; //anywhere
+        endPos = transform.position; //Distance will dynamically determine its ending position
     }
 
 
---
+++ Assets/Scripts/AI/EnemyAiV2.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAiV2 : MonoBehaviour {
+
+    [Space(5)]
+    [Header("Movement")]
+    public float posDistance = 0.2f;
+    public float posSpeed = 1;
+    public bool canMove = true;
+
+    private Vector3 startPos, leftPos, rightPos;
+
+    // Use this for initialization
+    void Awake()
+    {
+        startPos = transform.position;
+
+        //left and right start pos
+        leftPos = startPos;
+        rightPos = startPos;
+    }
+    void Start()
+    {
+        leftPos.x = startPos.x + posDistance; //will give coordinates to the left position
+        rightPos.x = startPos.x - posDistance; //rightPos coordinates
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canMove)
+        {
+            horizontalMovement();
+        }
+    }
+
+    //left and right
+    void horizontalMovement()
+    {
+        //This will move the passenger left and right 
+        //make conditions for when he reaches its destination and returns back to his orignal position and so on.
+        transform.position = Vector3.Lerp(leftPos, rightPos, Mathf.PingPong(Time.time * posSpeed, 1.0f));
+    }
+}
