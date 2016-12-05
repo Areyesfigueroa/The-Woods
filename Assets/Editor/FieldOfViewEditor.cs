cdce60f9235e7c347889c825fc2c095a4be0615c
Aliel <alielreyes@gmail.com>
2016-12-05 00:27:03 -0800

    Added3D Field Of view
    Translating it to 2D field of view

---
+++ Assets/Editor/FieldOfViewEditor.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(FieldOfView))]
+public class FieldOfViewEditor : Editor {
+
+    //Draws the Angles
+    //Change to 2D angle later
+    void OnSceneGUI()
+    {
+        FieldOfView fow = (FieldOfView)target;
+        Handles.color = Color.white;
+        
+        //Draws the Radius
+        Handles.DrawWireArc(fow.transform.position, Vector3.up, Vector3.forward, 360, fow.viewRadius);
+
+        //Sets up angles
+        Vector3 viewAngleA = fow.DirFromAngle(-fow.viewAngle / 2, false);
+        Vector3 viewAngleB = fow.DirFromAngle(fow.viewAngle / 2, false);
+
+        //Draws the Angle
+        Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleA * fow.viewRadius);
+        Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleB * fow.viewRadius);
+
+        Handles.color = Color.red;
+        foreach (Transform visibleTarget in fow.visibleTargets)
+        {
+            Handles.DrawLine(fow.transform.position, visibleTarget.position);
+        }
+    }
+}
